887885ddbc384c7dc35391cd422c8c45fb844c34
Robert Samoraj <rsamoraj11@gmail.com>
2023-07-28 21:15:36 -0500

    Create menu options and add option to see the default directory in the db.json config file

--- Setup_DB/setup_db.cpp
+++ Setup_DB/setup_db.cpp
@@ -3,19 +3,57 @@
 #include <iomanip>
 #include <sqlite3.h>
 #include <filesystem>
+#include <nlohmann/json.hpp>
+#include <fstream>
 #include "Db.h"
 
 using namespace std;
 namespace fs = std::filesystem;
+using json = nlohmann::json;
+
+int getMenuInput();
 
 int main(int argc, char* argv[])
 {
 	char overwrite;
 	//string name;
 	//string db_path = dir.append(name);
-	auto path = fs::path("C:\\Users\\rsamo\\test\\");
+	auto path = fs::path{ R"(C:\Users\rsamo\test\)"s};
+	ifstream infile({ R"(C:\Users\rsamo\source\config_files\db.json)"s });
+
+	json data = json::parse(infile);
+	string defaultDir = data["DefaultDbPath"];
 
 	cout << setw(45) << "##### Setup Database #####\n\n";
+
+
+	while (true)
+	{
+		int menuChoice = getMenuInput();
+	
+		switch (menuChoice)
+		{
+		case 1:
+			cout << "\u001b[2J";
+			cout << defaultDir << endl;
+			cout << "\n";
+
+			menuChoice = getMenuInput();
+			
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		default:
+			break;
+		}
+
+
+	}
 
 	Db db;
 
@@ -25,7 +63,7 @@ int main(int argc, char* argv[])
 
 	if (fs::exists(path))
 	{
-		cout << "Database " << db.getName() << " all ready exists at " << path.make_preferred() << endl;
+		cout << "Database " << db.getName() << " all ready exists at " << path.string() << endl;
 
 		cout << "\nOverwrite existing database? (WARNING: All previous data will be removed)\n";
 		cout << "Enter choice (Y/N): ";
@@ -39,14 +77,24 @@ int main(int argc, char* argv[])
 
 	if ((!(fs::exists(path))) || ((fs::exists(path) && tolower(overwrite) == 'y')))
 	{
-		cout << "WOO" << endl;
+		db.createDatabase();
 	}
 
 	//cout << db.getName() << endl;
 
-
-
-
-
 	return 0;
+}
+
+int getMenuInput()
+{
+	int menuInput;
+
+	cout << "1. View default database location\n"
+		<< "2. Choose new database directory\n"
+		<< "3. Exit\n\n";
+
+	cout << "Enter choice (1-3): ";
+	cin >> menuInput;
+
+	return menuInput;
 }
